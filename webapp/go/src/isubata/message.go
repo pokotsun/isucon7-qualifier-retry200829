9b4c76b481fbca62638312cad9a7fa7819bed638
naoto67 <uver.com67@gmail.com>
2020-08-29 13:25:05 +0900

    getMessageの中のN+1を削除

---
+++ webapp/go/src/isubata/message.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+)
+
+func getMessage(c echo.Context) error {
+	userID := sessUserID(c)
+	if userID == 0 {
+		return c.NoContent(http.StatusForbidden)
+	}
+
+	chanID, err := strconv.ParseInt(c.QueryParam("channel_id"), 10, 64)
+	if err != nil {
+		return err
+	}
+	lastID, err := strconv.ParseInt(c.QueryParam("last_message_id"), 10, 64)
+	if err != nil {
+		return err
+	}
+
+	messages, err := queryMessagesWithUser(chanID, lastID)
+	if err != nil {
+		return err
+	}
+
+	response := make([]map[string]interface{}, 0)
+	for i := len(messages) - 1; i >= 0; i-- {
+		m := messages[i]
+		r := make(map[string]interface{})
+		r["id"] = m.ID
+		r["user"] = User{Name: m.Name, DisplayName: m.DisplayName, AvatarIcon: m.AvatarIcon}
+		r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
+		r["content"] = m.Content
+		response = append(response, r)
+	}
+
+	if len(messages) > 0 {
+		_, err := db.Exec("INSERT INTO haveread (user_id, channel_id, message_id, updated_at, created_at)"+
+			" VALUES (?, ?, ?, NOW(), NOW())"+
+			" ON DUPLICATE KEY UPDATE message_id = ?, updated_at = NOW()",
+			userID, chanID, messages[0].ID, messages[0].ID)
+		if err != nil {
+			return err
+		}
+	}
+
+	return c.JSON(http.StatusOK, response)
+}
+
+func queryMessagesWithUser(chanID, lastID int64) ([]MessageUser, error) {
+	msgs := []MessageUser{}
+	err := db.Select(&msgs, "SELECT m.*, u.name, u.display_name, u.avatar_icon FROM message m INNER JOIN user u ON m.user_id = u.id WHERE m.id > ? AND m.channel_id = ? ORDER BY m.id DESC LIMIT 100",
+		lastID, chanID)
+	return msgs, err
+}
---
+++ webapp/go/src/isubata/util.go
@@ -0,0 +1,17 @@
+package main
+
+func jsonifyMessage(m Message) (map[string]interface{}, error) {
+	u := User{}
+	err := db.Get(&u, "SELECT name, display_name, avatar_icon FROM user WHERE id = ?",
+		m.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	r := make(map[string]interface{})
+	r["id"] = m.ID
+	r["user"] = u
+	r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
+	r["content"] = m.Content
+	return r, nil
+}
