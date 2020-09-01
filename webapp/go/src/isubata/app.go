11a78a59755420c8ea9570f392584c8514b6ab1f
pokotsun <gorigolila@gmail.com>
2020-09-01 22:56:19 +0900

    get channel message count from cache

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -432,8 +432,7 @@ func getHistory(c echo.Context) error {
 	}
 
 	const N = 20
-	var cnt int64
-	err = db.Get(&cnt, "SELECT COUNT(*) as cnt FROM message WHERE channel_id = ?", chID)
+	cnt, err := getChannelCount(cacheClient, chID)
 	if err != nil {
 		return err
 	}
